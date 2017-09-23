44b6e15c0d0f48f31b6487cb500537abbc512106
baioni1 <jbaioni23@gmail.com>
2017-09-22 20:36:36 -0400

    created board

---
+++ src/minesweeper.js
@@ -0,0 +1,19 @@
+const printBoard = (board) =>
+{
+  console.log('Current Board:');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+
+let board =
+[
+  [' ',' ',' '],
+  [' ',' ',' '],
+  [' ',' ',' ']
+];
+
+console.log(printBoard(board));
+board[0].splice(1,1,'1');
+board[2].splice(2,1,'B');
+console.log(printBoard(board));
